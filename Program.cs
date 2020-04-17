ac26520ca51490c3a729f0b17f7dbcfd888967df
unknown <yourGitHub@example.com>
2020-04-18 00:05:05 +0500

    The end of my program

--- Program.cs
+++ Program.cs
@@ -7,7 +7,36 @@ namespace MyprojecsApp
     {
         static void Main(string[] args)
         {
+            string[] ArrayString = { "a", "b", "c", "d", "е", "f" };
+            int[] ArrayInt = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            double[] ArrayDouble = { 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9 };
+            Decimal[] ArrayDecimal = { 2.1m, 2.2m, 2.3m, 2.4m, 2.5m, 2.6m, 2.7m };
+            float[] ArrayFloat = { 6.1f, 6.2f, 6.3f, 6.4f, 6.5f, 5.6f, 6.7f, 6.8f, 6.9f };
+            Console.WriteLine($"Removed item from array ArrayString: {ArrayHelper.Pop(ref ArrayString)}");
+            Console.WriteLine($"Removed item from array ArrayInt: {ArrayHelper.Pop(ref ArrayInt)}");
+            Console.WriteLine($"Removed item from array ArrayDouble: {ArrayHelper.Pop(ref ArrayDouble)}");
+            Console.WriteLine($"Removed item from array ArrayDecimal: {ArrayHelper.Pop(ref ArrayDecimal)}");
+            Console.WriteLine($"Removed item from array ArrayFloat: {ArrayHelper.Pop(ref ArrayFloat)}\n\n");
 
+            Console.WriteLine($"The length of the ArrayString array after adding another element from the end of the array: {ArrayHelper.Push(ref ArrayString, "J")}");
+            Console.WriteLine($"The length of the ArrayInt array after adding another element from the end of the array: {ArrayHelper.Push(ref ArrayInt, 11)}");
+            Console.WriteLine($"The length of the ArrayDouble array after adding another element from the end of the array: {ArrayHelper.Push(ref ArrayDouble, 6.0)}");
+            Console.WriteLine($"The length of the ArrayDecimal array after adding another element from the end of the array: {ArrayHelper.Push(ref ArrayDecimal, 2.8m)}");
+            Console.WriteLine($"The length of the ArrayFloat array after adding another element from the end of the array: {ArrayHelper.Push(ref ArrayFloat, 7.0f)}\n\n");
+
+            Console.WriteLine($"Removed item from array ArrayString: {ArrayHelper.Shift(ref ArrayString)}");
+            Console.WriteLine($"Removed item from array ArrayInt: {ArrayHelper.Shift(ref ArrayInt)}");
+            Console.WriteLine($"Removed item from array ArrayDouble: {ArrayHelper.Shift(ref ArrayDouble)}");
+            Console.WriteLine($"Removed item from array ArrayDecimal: {ArrayHelper.Shift(ref ArrayDecimal)}");
+            Console.WriteLine($"Removed item from array ArrayFloat: {ArrayHelper.Shift(ref ArrayFloat)}\n\n");
+
+            Console.WriteLine($"The length of the ArrayString array after adding another element to the beginning of the array: {ArrayHelper.UnShift(ref ArrayString, "Aa")}");
+            Console.WriteLine($"The length of the ArrayInt array after adding another element to the beginning of the array: {ArrayHelper.UnShift(ref ArrayInt, 0)}");
+            Console.WriteLine($"The length of the ArrayDouble array after adding another element to the beginning of the array: {ArrayHelper.UnShift(ref ArrayDouble, 5.0)}");
+            Console.WriteLine($"The length of the ArrayDecimal array after adding another element to the beginning of the array: {ArrayHelper.UnShift(ref ArrayDecimal, 2.0m)}");
+            Console.WriteLine($"The length of the ArrayFloat array after adding another element to the beginning of the array: {ArrayHelper.UnShift(ref ArrayFloat, 6.0f)}\n\n");
+            Console.ReadKey();
+            System.Console.WriteLine("");
         }
     }
     static class ArrayHelper
